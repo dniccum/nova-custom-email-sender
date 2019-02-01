bff81f1e578ee4272ab70914b7c08ca2ab6c8db9
Doug Niccum <dniccumdesign@gmail.com>
2019-02-01 10:09:40 -0600

    Adds additional settings for the editor

--- config/novaemailsender.php
+++ config/novaemailsender.php
@@ -50,14 +50,17 @@ return [
     | Email Template
     |--------------------------------------------------------------------------
     |
-    | To use the "send all" feature, define the model class and the accompanying
-    | properties to populate the message with.
+    | The Blade template used to send the email. By default this template
+    | utilizes the existing notification layout template. So any modifications
+    | that you have made to this layout template (like the header, colors, etc)
+    | will be inherited by the view supplied by this package.
+
+    | IMPORTANT: If you decide to supply your own Blade template, make sure that
+    | it includes a {!! $content !!} tag as this is what is used to parse the
+    | WYSIWYG's content
     |
     */
 
-    'template' => [
-        'markdown' => true,
-        'view' => resource_path('vendor/custom-email-sender/email')
-    ],
+    'template' => resource_path('vendor/custom-email-sender/email'),
 
 ];
