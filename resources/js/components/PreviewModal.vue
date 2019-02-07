46b6bf3eb7283e2b388a4b2be2a072cc1ce25fab
Doug Niccum <dniccumdesign@gmail.com>
2019-02-07 09:15:13 -0600

    Moves the success panel to a separate component, and then creates a preview modal to show the email

---
+++ resources/js/components/PreviewModal.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="modal-wrapper fixed w-screen h-screen pin z-30 flex flex-col items-center justify-center" v-show="visible">
+        <div class="background absolute w-full h-full" style="background-color: var(--primary); opacity: .8"></div>
+        <button class="btn btn-default btn-white text-primary z-10 mb-8" type="button" @click="close">
+            Close
+        </button>
+        <div class="iframe-wrapper relative z-10">
+            <iframe id="preview-frame" class="absolute w-full h-full pin rounded-lg"></iframe>
+        </div>
+        <button class="btn btn-default btn-white text-primary z-10 mt-8" type="button" @click="close">
+            Close
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PreviewModal",
+        data() {
+            return {
+                visible: false
+            }
+        },
+        mounted() {
+            let vm = this;
+
+            Nova.$on('show-email-preview', (response) => {
+                vm.visible = true;
+                document.getElementById('preview-frame').contentWindow.document.write(response);
+            })
+        },
+        methods: {
+            close() {
+                this.visible = false;
+                document.getElementById('preview-frame').contentWindow.document.body.innerHTML = '';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .iframe-wrapper {
+        width: 600px;
+        height: calc(80vh - 60px);
+    }
+</style>
--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -5,18 +5,22 @@
         <message-form
                 :quill-configuration="config.editor"
                 v-else></message-form>
+
+        <preview-modal></preview-modal>
     </div>
 </template>
 
 <script>
     import MessageForm from './MessageForm';
     import Loader from './Loader';
+    import PreviewModal from './PreviewModal';
 
     export default {
         name: 'CustomEmailSender',
         components: {
             MessageForm,
             Loader,
+            PreviewModal,
         },
         data() {
             return {
