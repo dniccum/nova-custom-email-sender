d677c6ff4620695c926ff3a82aa3b63640dbf84c
Doug Niccum <dniccumdesign@gmail.com>
2020-11-28 22:20:25 -0600

    Builds the structure and content.

--- resources/js/components/NebulaSender/EmailCard.vue
+++ resources/js/components/NebulaSender/EmailCard.vue
@@ -1,17 +1,56 @@
 <template>
-    <card class="px-6 py-4">
-        <div class="font-bold mb-2">
-            Subject Line
+    <card class="px-6 py-4" :class="{ 'active' : active }">
+        <div class="mb-3">
+            <div class="font-bold mb-1">
+                {{ message.subject }}
+            </div>
+            <div class="text-80 font-light font-italic text-xs">
+                {{ timestamp(message.created) }}
+            </div>
         </div>
         <div class="font-light text-90 text-sm">
-            Copy
+            {{ contentPreview }}
         </div>
+        <div class="card-click-overlay" @click="select"></div>
     </card>
 </template>
 
 <script>
+    import Timestamp from "../../mixins/Timestamp";
+
     export default {
-        name: "EmailCard"
+        name: "EmailCard",
+        mixins: [
+            Timestamp,
+        ],
+        props: {
+            active: {
+                type: Boolean,
+                default: false
+            },
+            message: {
+                type: Object,
+                required: true,
+            },
+        },
+        computed: {
+            /**
+             * @return {string}
+             */
+            contentPreview() {
+                if (this.message.content && this.message.content.length > 0) {
+                    let strippedContent = this.$options.filters.stripped(this.message.content);
+                    return this.$options.filters.limit(strippedContent, 80);
+                }
+
+                return '';
+            }
+        },
+        methods: {
+            select() {
+                this.$emit('show', this.message);
+            }
+        }
     }
 </script>
 
