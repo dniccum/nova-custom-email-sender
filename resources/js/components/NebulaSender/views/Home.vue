b854480e809d02f422f739da5f6eb51adc02707c
Doug Niccum <dniccumdesign@gmail.com>
2020-11-28 22:23:07 -0600

    Updates component with a dictionary call

--- resources/js/components/NebulaSender/views/Home.vue
+++ resources/js/components/NebulaSender/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="flex-1 flex items-center justify-center">
         <div class="font-medium text-60 text-4xl">
-            Select "Compose" to create a new message
+            {{ messages['select-compose-to-create-message'] }}
         </div>
     </div>
 </template>
 
 <script>
-    import NebulaSenderLayout from "../NebulaSenderLayout";
+    import Translations from "../../../mixins/Translations";
 
     export default {
         name: "Home",
-        components: {
-            NebulaSenderLayout,
-        }
+        mixins: [
+            Translations
+        ]
     }
 </script>
 
