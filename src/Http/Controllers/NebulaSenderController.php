9e88501bb246fde966f627b01d8ecce393fa8e8b
Doug Niccum <dniccumdesign@gmail.com>
2020-11-28 22:22:34 -0600

    Adds route to get all of the available messages.

--- src/Http/Controllers/NebulaSenderController.php
+++ src/Http/Controllers/NebulaSenderController.php
@@ -12,11 +12,17 @@ class NebulaSenderController
     private $apiRoute;
 
     /**
+     * @var string
+     */
+    private $key;
+
+    /**
      * NebulaSenderController constructor.
      */
     public function __construct()
     {
         $this->apiRoute = env('NEBULA_SENDER_API_ROUTE', 'https://nebulasender.com/api');
+        $this->key = config('novaemailsender.nebula_sender.key');
     }
 
     /**
@@ -25,8 +31,22 @@ class NebulaSenderController
      */
     public function drafts(Request $request)
     {
-        $response = Http::withToken(config('novaemailsender.nebula_sender_key'))
+        $response = Http::withToken($this->key)
             ->get($this->apiRoute.'/draft');
+
+        return response()->json([
+            'data' => $response->json('data')
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function messages(Request $request)
+    {
+        $response = Http::withToken($this->key)
+            ->get($this->apiRoute.'/message');
 
         return response()->json([
             'data' => $response->json('data')
